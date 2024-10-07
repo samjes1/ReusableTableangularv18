57e37dd150cd4f1e04d38740ff5fa74b980e2ef4
Samys Molina <samyjes4@gmail.com>
2024-10-07 10:09:29 -0400

    config angular with tailwind and flowbite

---
+++ src/app/components/table-data/table-data.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-table-data',
+  standalone: true,
+  imports: [],
+  templateUrl: './table-data.component.html',
+  styleUrl: './table-data.component.css'
+})
+export class TableDataComponent {
+
+}
